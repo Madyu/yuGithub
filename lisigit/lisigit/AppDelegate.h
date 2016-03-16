d153e374dcd46563e13bb5df54dc135ce7f7d4e4
yu <765853338@qq.com>
2016-03-16 10:21:45 +0800

    hehehe

---
+++ lisigit/lisigit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lisigit
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 Madyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lisigit/lisigit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lisigit
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 Madyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
